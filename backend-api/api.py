834a7f4912c33ee149d21315c92b6616db020502
Eric Li <e226li@uwaterloo.ca>
2023-11-16 12:43:21 -0500

    Switch typing to pydantic

--- backend-api/api.py
+++ backend-api/api.py
@@ -4,11 +4,17 @@ from fastapi import FastAPI, HTTPException, Security, status
 from fastapi.security import APIKeyHeader
 import yaml
 from databases import Database
+from pydantic import BaseModel
 
 
-class Count(NamedTuple):
+class Count(BaseModel):
     wifi: int
     bluetooth: int
+
+
+class Location(BaseModel):
+    lat: float
+    long: float
 
 
 @asynccontextmanager
@@ -43,11 +49,12 @@ async def root():
 
 
 @app.post("/data/", status_code=204)
-async def save_data(id: int, timestamp_unix_epoch: int, location: Tuple[float, float], device_count: Count):
+async def save_data(id: int, timestamp_unix_epoch: int, location: Location, device_count: Count):
+    corrected_device_count = int((device_count.bluetooth + device_count.wifi) / 0.7)
+
     # enforced types should mean no sql injection occurs
     query = "INSERT INTO device_data VALUES(:id, :timestamp, :lat, :long, :count)"
-    corrected_device_count = (device_count.bluetooth + device_count.wifi) / 0.7
-    values = {'id': id, 'timestamp': timestamp_unix_epoch, 'lat': location[0], 'long': location[1],
+    values = {'id': id, 'timestamp': timestamp_unix_epoch, 'lat': location.lat, 'long': location.long,
               'count': corrected_device_count}
 
     await database.execute(query=query, values=values)
