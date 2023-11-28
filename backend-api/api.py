1dbecdcedee22251e410f7f53eb2e09c3bbf940e
Eric Li <e226li@uwaterloo.ca>
2023-11-28 15:17:07 -0500

    Refactor schema (get_density split, save_data reworked)

--- backend-api/api.py
+++ backend-api/api.py
@@ -1,7 +1,8 @@
 import os
+import time
 from collections import defaultdict
 from datetime import datetime, timedelta
-from typing import Tuple, NamedTuple
+from typing import Tuple, NamedTuple, List
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, HTTPException, Security, status
 from fastapi.middleware.cors import CORSMiddleware
@@ -12,19 +13,13 @@ from pydantic import BaseModel
 from starlette.responses import RedirectResponse
 
 
-class Count(BaseModel):
-    wifi: int
-    bluetooth: int
-
-
-class Location(BaseModel):
-    lat: float
-    long: float
+class Detected(BaseModel):
+    mac: List[str]
+    rssi: List[str]
 
 
 class Density(BaseModel):
-    average: dict = dict()
-    latest: dict = dict()
+    density: dict = dict()
     current_hour: int = datetime.now().hour
 
 
@@ -71,54 +66,73 @@ def get_api_key(api_key_header: str = Security(api_key_header)) -> str:
     )
 
 
+async def fetch_from_db(target_hour, now_hour):
+    current_hour_epoch = now_hour.timestamp() + target_hour * 3600 * 1000
+    query = "SELECT * FROM device_data WHERE timestamp >= :start AND timestamp <= :stop;"
+    values = {'start': current_hour_epoch, 'stop': current_hour_epoch + 3600 * 1000}
+    return_values = await database.fetch_all(query=query, values=values)
+    return return_values
+
+
 @app.get("/", include_in_schema=False)
 async def root():
     return RedirectResponse("docs")
 
 
 @app.post("/data/", status_code=204)
-async def save_data(device_id: int, timestamp_unix_epoch: int, location: Location, device_count: Count,
+async def save_data(device_id: int,  device_count: int, detected: Detected,
                     api_key: str = Security(get_api_key)):
-    corrected_device_count = int((device_count.bluetooth + device_count.wifi) / 0.7)
-
     # enforced types should mean no sql injection occurs
-    query = "INSERT INTO device_data VALUES(:device_id, :timestamp, :lat, :long, :count)"
-    values = {'id': device_id, 'timestamp': timestamp_unix_epoch, 'lat': location.lat, 'long': location.long,
-              'count': corrected_device_count}
+    query = "INSERT INTO device_data VALUES(:id, :timestamp, :lat, :long, :count)"
+    values = {'id': device_id, 'timestamp': time.time(), 'lat': 0, 'long': 0,
+              'count': device_count}
 
     await database.execute(query=query, values=values)
     return
 
 
-@app.get("/get-density/")
-async def get_density(api_key: str = Security(get_api_key)) -> Density:
+@app.get("/get-average-density/")
+async def get_average_density(api_key: str = Security(get_api_key)) -> Density:
     density = Density()
 
     now = datetime.now()
     now_hour = now.replace(second=0, microsecond=0, minute=0, hour=now.hour) + timedelta(hours=now.minute//30)
     for hour in range(24):
         current_hour = (now_hour.hour - hour - 1) % 24
-        current_hour_epoch = now_hour.timestamp() + hour * 3600 * 1000
+        return_values = await fetch_from_db(hour, now_hour)
 
-        query = "SELECT * FROM device_data WHERE timestamp >= :start AND timestamp <= :stop;"
-        values = {'start': current_hour_epoch, 'stop': current_hour_epoch + 3600*1000}
-
-        return_values = await database.fetch_all(query=query, values=values)
         data_sum = defaultdict(int)
         data_num = defaultdict(int)
-        last_run_time = defaultdict(int)
-        last_run_count = defaultdict(int)
         for value in return_values:
             data_sum[value['id']] += value['count']
             data_num[value['id']] += 1
+
+        density.density[current_hour] = dict()
+        for device_id in data_sum.keys():
+            density.density[current_hour][device_id] = data_sum[device_id] / data_num[device_id]
+
+    return density
+
+
+@app.get("/get-latest-density/")
+async def get_latest_density(api_key: str = Security(get_api_key)) -> Density:
+    density = Density()
+
+    now = datetime.now()
+    now_hour = now.replace(second=0, microsecond=0, minute=0, hour=now.hour) + timedelta(hours=now.minute//30)
+    for hour in range(24):
+        current_hour = (now_hour.hour - hour - 1) % 24
+        return_values = await fetch_from_db(hour, now_hour)
+
+        last_run_time = defaultdict(int)
+        last_run_count = defaultdict(int)
+        for value in return_values:
             if value['timestamp'] > last_run_time[value['id']]:
                 last_run_time[value['id']] = value['timestamp']
                 last_run_count[value['id']] = value['count']
 
-        density.average[current_hour] = dict()
-        for device_id in data_sum.keys():
-            density.average[current_hour][device_id] = data_sum[device_id] / data_num[device_id]
+        for device_id in last_run_time.keys():
             if current_hour == now_hour.hour:
-                density.latest[device_id] = last_run_count[device_id]
+                density.density[device_id] = last_run_count[device_id]
 
     return density
