3895e31fc6fcd0090ad88a36967152f31ad331ad
Eric Li <e226li@uwaterloo.ca>
2023-11-29 10:45:37 -0500

    Replace hyphens with underscores for imports

---
+++ backend_api/tests/test_api.py
@@ -0,0 +1,23 @@
+import unittest
+from fastapi.testclient import TestClient
+from ..api import app
+
+client = TestClient(app)
+
+
+class TestBackend(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_example(self):
+        pass
+
+    def test_example_2(self):
+        response = client.get("/items/foo", headers={"X-Token": "baz"})
+        assert response.status_code == 200
+        assert response.json() == {
+            "id": "foo",
+            "title": "Foo",
+            "description": "bar",
+        }
+    
