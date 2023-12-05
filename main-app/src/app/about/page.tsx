a585e1868bf019c5ea26df807c2bd610e1f347a1
alexstarosta <alexstarostadev@gmail.com>
2023-12-04 23:49:31 -0500

    about page reactive styling

--- main-app/src/app/about/page.tsx
+++ main-app/src/app/about/page.tsx
@@ -34,32 +34,32 @@ export default function Home() {
     <div className="w-[100vw] h-[100vh] overflow-hidden">
         <NavigationBar />
         <div className="my-auto h-[80vh] flex flex-col justify-center items-center gap-3">
-            <div className="flex flex-row gap-10">
+            <div className="flex md:flex-row flex-col gap-10">
               <Image 
-                className="w-auto h-auto"
+                className="w-auto h-auto scale-75 md:scale-100"
                 src="/logo.webp"
                 alt="UW Map Logo"
                 width={200}
                 height={200}
                 priority={true}
               />
-              <div className="my-auto gap-3 flex flex-col">
-                <div className="flex flex-row items-center gap-[4px]">
-                    <div className="font-semibold text-8xl">UW</div>
-                    <div className="font-semibold text-8xl bg-clip-text text-transparent bg-gradient-to-br from-lightblue to-blue-500">Map</div>
+              <div className="my-auto gap-3 flex flex-col justify-center text-center md:text-left md:justify-start mt-[-50px] md:mt-0">
+                <div className="flex flex-row items-center gap-[4px] justify-center md:justify-start">
+                    <div className="font-semibold md:text-8xl text-6xl">UW</div>
+                    <div className="font-semibold md:text-8xl text-6xl bg-clip-text text-transparent bg-gradient-to-br from-lightblue to-blue-500">Map</div>
                 </div>
-                <h2 className="text-2xl font-semibold">A modern map for a modern campus.</h2>
+                <h2 className="md:text-2xl text-lg font-semibold">A modern map for a modern campus.</h2>
               </div>
             </div>
         </div>
-        <div className="flex flex-row justify-center items-center gap-2">
-          <div className="text-lg">A project</div>
+        <div className="flex flex-row justify-center items-center gap-2 text-[11px] md:text-lg">
+          <div>A project</div>
           <code className="font-mono bg-clip-text text-transparent bg-gradient-to-br from-lightblue to-blue-500">
             <Suspense fallback={null}>{formatter.format(getAge(time))}</Suspense>
           </code>
-          <div className="text-lg">hours in the making, made by</div>
+          <div>hours in the making, made by</div>
         </div>
-        <div className="flex flex-row justify-center items-center gap-2 text-lg">
+        <div className="flex flex-row justify-center items-center text-[10px] md:text-lg md:gap-2 gap-1">
           <div>Anthony Chen,</div>
           <div>Christine Li,</div>
           <div>Eric Li,</div>
